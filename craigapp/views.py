05cafa845b3a3dd750b1e6874ca2bae785af26e8
chucklapress <charleslapress@gmail.com>
2016-06-24 17:17:34 -0400

    changes to views and index

--- craigapp/views.py
+++ craigapp/views.py
@@ -1,12 +1,16 @@
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import CreateView
 
-from craigapp.models import Listing
+from craigapp.models import Listing, Category
 
 
 class IndexView(CreateView):
     template_name = 'index.html'
     model = Listing
-    fields = ["category"]
+    fields = ["category","shopper","item_price"]
+
+    def get_queryset(self):
+        return Listing.objects.all()
