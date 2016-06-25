5b174d0a9b138e1be0622fc46ba2628a685727a8
chucklapress <charleslapress@gmail.com>
2016-06-25 12:31:04 -0400

    changed templateview to list view to render to indexpage

--- craigapp/views.py
+++ craigapp/views.py
@@ -2,15 +2,17 @@ from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView
+from django.views.generic import ListView
 
 from craigapp.models import Listing, Category
 
 
-class IndexView(CreateView):
+class IndexView(ListView):
     template_name = 'index.html'
     model = Listing
-    fields = ["category","shopper","item_price"]
 
     def get_queryset(self):
         return Listing.objects.all()
+
+
+
