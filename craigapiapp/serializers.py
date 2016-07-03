cbcbddc6756fe96008733d396a5cd7561ad3675c
chucklapress <charleslapress@gmail.com>
2016-07-03 09:19:27 -0400

    added hidden user in serializers to allow permissions to work

--- craigapiapp/serializers.py
+++ craigapiapp/serializers.py
@@ -5,27 +5,35 @@ from craigapp.models import Listing, Category, SubCategory, Region
 
 
 class ListingSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
 
     class Meta:
         model = Listing
-        fields = ['id', 'category','shopper','item_price']
+        fields = ['id', 'category','shopper','item_price','user']
 
 class CategorySerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
 
     class Meta:
         model = Category
-        fields = ['id','name','item_location','post_date']
+        fields = ['id','name','item_location','post_date','user']
 
 class SubCategorySerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
 
     class Meta:
         model = SubCategory
-        fields = ['id','name','category']
+        fields = ['id','name','category','user']
         depth = 1
 
 class RegionSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
 
     class Meta:
         model = Region
-        fields = ['id','location','location_name']
+        fields = ['id','location','location_name','user']
         depth = 1
