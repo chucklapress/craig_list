fedca32ea42951089d7bf769e962c1368898cec7
chucklapress <charleslapress@gmail.com>
2016-07-04 10:44:08 -0400

    added methods for user requirements in create for apiviews

--- craigapiapp/serializers.py
+++ craigapiapp/serializers.py
@@ -6,33 +6,76 @@ from craigapp.models import Listing, Category, SubCategory, Region
 
 class ListingSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
+    user = serializers.CharField(allow_blank=True, required=False)
     class Meta:
         model = Listing
         fields = ['id', 'category','shopper','item_price','user']
+        def validate_user(self, value):
+
+            if value and len(value) > 0:
+                raise serializers.ValidationError('Error')
+                return value
+
+    def create(self, validated_data):
+        if "user" in validated_data:
+            del validated_data["user"]
+        return Listing.objects.create(**validated_data)
+
 
 class CategorySerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
+    user = serializers.CharField(allow_blank=True, required=False)
 
     class Meta:
         model = Category
         fields = ['id','name','item_location','post_date','user']
+        def validate_user(self, value):
+
+            if value and len(value) > 0:
+                raise serializers.ValidationError('Error')
+                return value
+
+    def create(self, validated_data):
+        if "user" in validated_data:
+            del validated_data["user"]
+        return Category.objects.create(**validated_data)
 
 class SubCategorySerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
+    user = serializers.CharField(allow_blank=True, required=False)
 
     class Meta:
         model = SubCategory
         fields = ['id','name','category','user']
         depth = 1
+        def validate_user(self, value):
+
+            if value and len(value) > 0:
+                raise serializers.ValidationError('Error')
+                return value
+
+    def create(self, validated_data):
+        if "user" in validated_data:
+            del validated_data["user"]
+        return SubCategory.objects.create(**validated_data)
+
 
 class RegionSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    user = serializers.CharField(allow_blank=True, required=False)
 
 
     class Meta:
         model = Region
         fields = ['id','location','location_name','user']
         depth = 1
+        def validate_user(self, value):
+
+            if value and len(value) > 0:
+                raise serializers.ValidationError('Error')
+                return value
+
+    def create(self, validated_data):
+        if "user" in validated_data:
+            del validated_data["user"]
+        return Region.objects.create(**validated_data)
